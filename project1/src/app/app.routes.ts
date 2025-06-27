4f99bed86c45854c208c88550fd0306f8e2a2e77
Youssef Hesham Mohamed Mohamed <youssef.mohamed@flairstech.com>
2025-06-27 22:32:58 +0300

    Updated code

--- project1/src/app/app.routes.ts
+++ project1/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { AboutComponent } from './about/about.component';
 import { StartframeworkComponent } from './startframework/startframework.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo:'startframework',pathMatch:'full'},
     { path: 'startframework', component: StartframeworkComponent },
     { path: 'about', component: AboutComponent },
     { path: 'portfolio', component: PortfolioComponent },
-    { path: 'contact', component: ContactComponent }
+    { path: 'contact', component: ContactComponent },
+    { path: '**', component: NotfoundComponent }
+    
     
 ];
