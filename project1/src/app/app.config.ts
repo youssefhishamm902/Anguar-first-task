54a743455068ae6510be05dc46e37a153572eb00
Youssef Hesham Mohamed Mohamed <youssef.mohamed@flairstech.com>
2025-06-27 23:29:32 +0300

    Updated one

--- project1/src/app/app.config.ts
+++ project1/src/app/app.config.ts
@@ -1,9 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation, withInMemoryScrolling, withViewTransitions } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withInMemoryScrolling ( {scrollPositionRestoration:'top'}) , withViewTransitions(),  withHashLocation()), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' }),
+      withViewTransitions(),
+      withHashLocation()
+    ),
+    provideClientHydration(withEventReplay())
+  ]
 };
